b317ee2cd66c155b14ff79995660f37eb08906d3
Young hoon Go <kyhsa93@naver.com>
2020-10-31 19:12:55 +0900

    Update: add stuff http controller

---
+++ stuff/api/controller.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	"encoding/json"
+	"github/four-servings/dropit-backend/stuff/app/command"
+	"net/http"
+)
+
+// NewController create controller instance
+func NewController(commandBus command.Bus) Controller {
+	return &controllerImplement{commandBus}
+}
+
+type (
+	// Controller http controller
+	Controller interface {
+		Handle(w http.ResponseWriter, r *http.Request)
+	}
+
+	controllerImplement struct {
+		commandBus command.Bus
+	}
+)
+
+// Handle handle http request
+func (c *controllerImplement) Handle(w http.ResponseWriter, r *http.Request) {
+	c.branchByMethod(w, r)
+}
+
+func (c *controllerImplement) branchByMethod(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		c.handlePOST(w, r)
+		return
+	default:
+		c.handleNotAllowedMethod(w, r)
+		return
+	}
+}
+
+type createStuffBody struct {
+	Name     string `json:"name"`
+	Category string `json:"category"`
+}
+
+func (c *controllerImplement) handlePOST(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	body := createStuffBody{}
+	if decoder.Decode(&body) != nil {
+		http.Error(w, "can not parse body", http.StatusBadRequest)
+		return
+	}
+
+	c.commandBus.Handle(&command.CreateStuff{body.Name, body.Category})
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (c *controllerImplement) handleNotAllowedMethod(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
